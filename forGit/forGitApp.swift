cba7ba425e50ee01a34352fd31065542a027ea78
AlloTheOne <Allo.busy@gmail.com>
2022-11-01 12:10:46 +0300

    first commit

---
+++ forGit/forGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  forGitApp.swift
+//  forGit
+//
+//  Created by Alaa Alabdullah on 01/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct forGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
